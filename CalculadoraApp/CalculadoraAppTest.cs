25d1d59c6343a1c512d0367db92d3ffbe892db11
Domingo Jesus Sanchez Blanco <domingojesus.sanchez316@comunidadunir.net>
2024-05-26 17:04:35 +0200

    Añadida nueva clase de prueba y método para sumar números
    Se eliminó la clase de prueba `UnitTest1` del archivo `UnitTest1.cs`. Se agregó una nueva clase de prueba `CalculadoraAppTest` en el archivo `CalculadoraAppTest.cs`. En esta nueva clase, se incluyó un método de prueba `ShouldSumTwoNumbers()` que verifica la funcionalidad de suma de la aplicación `CalculadoraApp`. Este método crea una instancia de `CalculadoraApp`, define dos números, los suma utilizando el método `Somar` y verifica si el resultado es correcto.

---
+++ CalculadoraApp/CalculadoraAppTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraApp.Core.Tests
+{
+    public class CalculadoraAppTest
+    {
+
+        public void ShouldSumTwoNumbers()
+        {
+            // Arrange
+            var calculadora = new CalculadoraApp();
+            var number1 = 10;
+            var number2 = 20;
+
+            // Act
+            var result = calculadora.Somar(number1, number2);
+
+            // Assert
+            Assert.AreEqual(30, result);
+        }
+    }
+}
