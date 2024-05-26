2fdde19a5e26c62c206234b8e689707af26b8464
Domingo Jesus Sanchez Blanco <domingojesus.sanchez316@comunidadunir.net>
2024-05-26 17:54:32 +0200

    Añadidas nuevas funciones y pruebas a Calculadora
    Se han implementado nuevas funciones en la clase `Calculadora` (`Multiplicar`, `Dividir`, `RaizCuadrada`) y se han añadido pruebas correspondientes en `CalculadoraAppTest`. Nota: la función `RaizCuadrada` actualmente lanza una excepción `NotImplementedException`.

--- CalculadoraApp/Calculadora.cs
+++ CalculadoraApp/Calculadora.cs
@@ -1,5 +1,7 @@
 ﻿
 
+
+
 namespace CalculadoraApp.Core.Tests
 {
     public class Calculadora
@@ -17,5 +19,58 @@ namespace CalculadoraApp.Core.Tests
         {
             return numero1 - numero2;
         }
+
+        public decimal Multiplicar(decimal numero1, decimal numero2)
+        {
+            return numero1 * numero2;
+        }
+
+        public decimal Dividir(decimal numero1, decimal numero2)
+        {
+            return numero1 / numero2;
+        }
+
+        public decimal RaizCuadrada(decimal number)
+        {
+            // This will be the nearest 
+            // perfect square to s
+            int pSq = 0;
+
+            //This is the sqrt of pSq
+            int N = 0;
+
+            // Find the nearest perfect square to s
+            for (int i = (int)(number); i > 0; i--)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    if (j * j == i)
+                    {
+                        pSq = i;
+                        N = j;
+                        break;
+                    }
+                }
+
+                if (pSq > 0)
+                    break;
+            }
+
+            // calculate d 
+            decimal d = number - pSq;
+
+            // calculate P
+            decimal P = d / (2.0m * N);
+
+            // calculate A
+            decimal A = N + P;
+
+            // calculate sqrt(S).
+            decimal sqrt_of_s = A - ((P * P) / (2.0m * A));
+
+            return sqrt_of_s;
+        }
+
+
     }
 }
--- CalculadoraApp/CalculadoraAppTest.cs
+++ CalculadoraApp/CalculadoraAppTest.cs
@@ -38,5 +38,53 @@ namespace CalculadoraApp.Core.Tests
             // Assert
             Assert.Equal(-9.9m, result);
         }
+
+        [Fact]
+        public void ShouldMultiplyTwoNumbers()
+        {
+            // Arrange
+            Calculadora calculadora = new Calculadora();
+            decimal number1 = 10.3m;
+            decimal number2 = 20.2m;
+
+            // Act
+            decimal result = calculadora.Multiplicar(number1, number2);
+
+            // Assert
+            Assert.Equal(208.06m, result);
+        }
+
+        [Fact]
+        public void ShouldDivideTwoNumbers()
+        {
+            // Arrange
+            Calculadora calculadora = new Calculadora();
+            decimal number1 = 10.3m;
+            decimal number2 = 20.2m;
+
+            // Act
+            decimal result = calculadora.Dividir(number1, number2);
+
+            // Assert
+            Assert.Equal(0.5099009900990099009900990099m, result);
+        }
+
+
+        [Fact]
+
+        public void SouldSquareRootANumber()
+        {
+            // Arrange
+            Calculadora calculadora = new Calculadora();
+            decimal number = 5.0m;
+            decimal expected = 2.2360679774997896964091736687m;
+
+
+            // Act
+            decimal result = calculadora.RaizCuadrada(number);
+
+            // Assert
+            Assert.True(Math.Abs(expected-result) < 0.001m);
+        }
     }
 }
